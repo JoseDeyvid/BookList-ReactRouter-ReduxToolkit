0fabcd577df0138876b3dc41244131652854828b
Jose Deyvid <j_deyvid@live.com>
2024-12-21 05:24:04 -0300

    Implementar logica de adicionar livro

--- src/routes/AddBook.tsx
+++ src/routes/AddBook.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import { useDispatch, useSelector } from "react-redux";
 import { addBook, booksSelector } from "../store/booksSlice";
@@ -7,14 +7,20 @@ import { Book } from "../utils/types";
 const AddBook = () => {
   const books = useSelector(booksSelector);
   const dispatch = useDispatch();
+
+  const [title, setTitle] = useState("");
+  const [cover, setCover] = useState("");
+  const [author, setAuthor] = useState("");
+  const [synopsis, setSynopsis] = useState("");
+
   const handleAddBook = () => {
     const newBook: Book = {
-      author: "author2",
+      author,
       id: books.length ? Math.max(...books.map((book) => book.id)) + 1 : 0,
-      image_url:
-        "https://lirp.cdn-website.com/4deba6da/dms3rep/multi/opt/fullsize-2018-01-09-18-Peca-Grafica-230330_470658_181301872_308451081-1e8eebeb-640w.jpg",
+      image_url: cover,
       read: false,
-      title: "title2",
+      title,
+      synopsis,
     };
 
     dispatch(addBook(newBook));
@@ -22,7 +28,43 @@ const AddBook = () => {
   return (
     <div>
       <Header />
-      <button onClick={handleAddBook}>Adicionar livro</button>
+      <h1>Adicionando novo livro</h1>
+      <div>
+        <label htmlFor="title">Título: *</label>
+        <input
+          type="text"
+          placeholder="Digite o título do livro..."
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="cover">Capa: *</label>
+        <input
+          type="text"
+          placeholder="Digite a url da capa do livro..."
+          value={cover}
+          onChange={(e) => setCover(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="author">Autor: *</label>
+        <input
+          type="text"
+          placeholder="Digite o autor do livro..."
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="synopsis">Título:</label>
+        <textarea
+          placeholder="Digite a sinopse do livro..."
+          value={synopsis}
+          onChange={(e) => setSynopsis(e.target.value)}
+        />
+      </div>
+      <button onClick={handleAddBook}>Salvar livro</button>
     </div>
   );
 };
