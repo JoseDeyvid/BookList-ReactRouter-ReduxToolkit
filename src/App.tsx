ffee0e833123b8cef77b1da1492757694f7ec57e
Jose Deyvid <j_deyvid@live.com>
2024-12-21 02:45:07 -0300

    Criar rotas e configurar react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 // import styles from "./App.module.scss";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import AddBook from "./routes/AddBook";
+import Book from "./routes/Book";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="add-book" element={<AddBook />} />
+          <Route path="book/:id" element={<Book />} />
+          <Route path="*" element={<h1>Page not found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
